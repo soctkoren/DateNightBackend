1d24e82196e81ba7793c92a5ff091a108e4b2346
soctkoren <joimkim@gmail.com>
2018-04-16 22:05:53 -0700

    add comments migrations

--- datenight_backend/models/comment.py
+++ datenight_backend/models/comment.py
@@ -1,5 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+from .node import Node
 
 class Comment(models.Model):
     content = models.TextField(max_length=280)
     is_hidden = models.BooleanField(default=True)
+    user = models.ForeignKey(User, default=1, on_delete=models.CASCADE)
+    node = models.ForeignKey(Node, default=1, on_delete=models.CASCADE)
