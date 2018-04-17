0789364b6df16dde1d4a181996157ab28fcdac84
soctkoren <joimkim@gmail.com>
2018-04-16 22:07:14 -0700

    add destination nodes migration

--- datenight_backend/models/destination_node.py
+++ datenight_backend/models/destination_node.py
@@ -1,4 +1,8 @@
 from django.db import models
+from .node import Node
+from .image import Image
 
 class DestinationNode(models.Model):
     description = models.CharField(max_length=200)
+    node = models.ForeignKey(Node, default=1, on_delete=models.CASCADE)
+    image = models.ForeignKey(Image, null=True, on_delete=models.CASCADE)
