e3ec2a471a25e3dd9b90291051561b0957d3af11
soctkoren <joimkim@gmail.com>
2018-04-16 22:01:29 -0700

    date img migrations

--- datenight_backend/models/image.py
+++ datenight_backend/models/image.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth import User
 
 class Image(models.Model):
     img_url = models.ImageField(upload_to='imgs/')
+    user = models.ForeignKey(User, default=1, on_delete=models.CASCADE)
