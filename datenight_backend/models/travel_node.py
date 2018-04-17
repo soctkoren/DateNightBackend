028532b82a53d8ee3a30e417f19e1a976e90633b
soctkoren <joimkim@gmail.com>
2018-04-16 22:08:12 -0700

    add travel nodes migrations

--- datenight_backend/models/travel_node.py
+++ datenight_backend/models/travel_node.py
@@ -1,5 +1,7 @@
 from django.db import models
+from .node import Node
 
 class TravelNode(models.Model):
     travel_method = models.TextField(max_length=None)
     estimated_travel_time = models.IntegerField
+    node = models.ForeignKey(Node, default=1, on_delete=models.CASCADE)
