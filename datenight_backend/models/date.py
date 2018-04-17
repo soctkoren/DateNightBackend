5fc87ad6aa6cacc7656416f273183caf13a7b6ac
soctkoren <joimkim@gmail.com>
2018-04-16 21:31:38 -0700

    add foregin key to date

--- datenight_backend/models/date.py
+++ datenight_backend/models/date.py
@@ -1,6 +1,8 @@
 from django.db import models
+from image import Image
 
 class Date(models.Model):
     date_description = models.CharField(max_length=200)
     is_public = models.BooleanField(default=False)
     is_created = models.BooleanField(default=False)
+    hero_img_id = models.ForeignKey(Image, on_delete=models.CASCADE)
