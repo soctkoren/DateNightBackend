95c28aa194967e1d411d88d505740b2653f41c05
soctkoren <joimkim@gmail.com>
2018-04-16 22:04:07 -0700

    add date_id to node migration

--- datenight_backend/models/node.py
+++ datenight_backend/models/node.py
@@ -1,5 +1,7 @@
 from django.db import models
+from .date import Date
 
 class Node(models.Model):
     order_number = models.SmallIntegerField()
     is_hidden = models.BooleanField(default=True)
+    date = models.ForeignKey(Date, null=True, on_delete=models.CASCADE)
